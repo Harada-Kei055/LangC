f47af2818bdd7c1830744c896d61a7b1cb2f0a5f
Harada-Kei055 <147792654+Harada-Kei055@users.noreply.github.com>
2023-11-21 12:21:29 +0900

    Add files via upload

---
+++ exercise1-2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int x=10,y,z;
+    y=2*x;
+    z=y+x;
+    printf("x=%d\ny=%d\nz=%d\n",x,y,z);
+    return 0;
+}
---
+++ exercise1-3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int  price=500;
+    float result;
+    result=price*1.1;
+    printf("出力結果:%0.2f",result);
+
+}
