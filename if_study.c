6f68386634bdae4884b41bb97f13ce7ab9905f77
Harada-Kei055 <147792654+Harada-Kei055@users.noreply.github.com>
2023-11-24 13:36:17 +0900

    Add files via upload

---
+++ if_study.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a=6;
+    if (a>5) {
+        printf("aは5より大きい\n");
+    }else {
+        printf("aは5より小さい\n");
+    }
+    return 0;
+}
---
+++ if_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int e = 8;
+    if(e<10){
+        printf("eは10未満です");
+    }
+    return 0;
+}
